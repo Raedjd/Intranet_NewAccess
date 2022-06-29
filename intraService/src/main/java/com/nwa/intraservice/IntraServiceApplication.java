f8a2a5a379c287e1035f21cf745033363167cb70
Raedjd <raed.jaidi@esprit.tn>
2022-06-29 13:58:21 +0100

    MicroService for Intranet created ✅

---
+++ intraService/src/main/java/com/nwa/intraservice/IntraServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nwa.intraservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntraServiceApplication.class, args);
+    }
+
+}
---
+++ intraService/src/test/java/com/nwa/intraservice/IntraServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwa.intraservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
