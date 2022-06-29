b906c5cb3359ee7e6bcc7e2331af3ada184bf3cd
Raedjd <raed.jaidi@esprit.tn>
2022-06-29 16:56:38 +0100

    Repository created ✅

--- intraService/src/main/java/com/nwa/intraservice/repository/UserRepository.java
+++ intraService/src/main/java/com/nwa/intraservice/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.nwa.intraservice.repository;
 
 import com.nwa.intraservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 }
